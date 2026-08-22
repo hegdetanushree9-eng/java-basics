4bee23887bae7cf1e2e612a53d11cfef9b71705c
hegdetanushree9-eng <hegdetanushree9@gmail.com>
2026-08-22 11:30:21 +0530

    Added Java basics program

---
+++ Program2.java
@@ -0,0 +1,16 @@
+public class Program2 {
+    public static void main(String[] args) {
+
+        int age = 19;
+        double height = 5.5;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Tanushree";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}

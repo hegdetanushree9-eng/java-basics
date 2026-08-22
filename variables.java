01ddbc487d3bb926dc55bd03737f228ea077b2a8
hegdetanushree9-eng <hegdetanushree9@gmail.com>
2026-08-22 11:23:36 +0530

    Add Java basics program

---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    public static void main(String[] args) {
+
+        System.out.println("Name: Tanushree Hegde");
+        System.out.println("Age: 19");
+        System.out.println("Branch: Data Science");
+        System.out.println("College: Sri Venkateshwara College of Engineering");
+    }
+}
